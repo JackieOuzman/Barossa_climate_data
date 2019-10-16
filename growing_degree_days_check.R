3ba9930f72984f756268da24d9df80f631d3ce1f
Ouzman <ouz001@csiro.au>
2019-10-16 13:36:26 +1100

    This is looking good much faster to run no bands on low values the GGD is very high - not sure if this is correct

--- growing_degree_days_check.R
+++ growing_degree_days_check.R
@@ -90,13 +90,16 @@ for (i in leap_years) {
 
 #function four
 function_GG_non_leap_yrs <- function(yr) {
-  min <- brick(
+  min_1 <- brick(
     paste("//af-osm-05-cdc.it.csiro.au/OSM_CBR_AF_CDP_work/silo/min_temp/",
           yr, ".min_temp.nc", sep = ""),varname = "min_temp")
   #bring in the max grid for year
-  max <- brick(
+  max_1 <- brick(
     paste("//af-osm-05-cdc.it.csiro.au/OSM_CBR_AF_CDP_work/silo/max_temp/",
           yr , ".max_temp.nc", sep = ""),varname = "max_temp")
+  min <- crop(min_1, barrossa_sf)
+  max <- crop(max_1, barrossa_sf)
+  
   #cal the mean daily temp for each grid cell per day 
   daily_mean_temp <- overlay(min, max, fun = function_daily_mean_temp)
   #retain only values greater than 10 using the below function
@@ -151,33 +154,15 @@ GDD_all_yrs <- stack(GGD_non_leap_yrs_1989,
                       GGD_non_leap_yrs_2018)
 
 
-
-GDD_all_yrs <- stack(GDD_all_leap_yrs, GDD_all_non_leap_yrs)
+GGD_non_leap_yrs_1989
+plot(GGD_non_leap_yrs_1989)
 
 mean_GDD_all_yrs <- calc(GDD_all_yrs, fun = mean, na.rm = T)
 mean_GDD_all_yrs
-
-
-library(sf)
-
-#barrossa_st <- st_read("//FSSA2-ADL/CLW-SHARE3/Viticulture/Barossa terroir/Vine_health_data/CSIRO/GI/ZONE/barossa_WGS.shp")
-barrossa_st <- st_read("//FSSA2-ADL/CLW-SHARE3/Viticulture/Barossa terroir/Vine_health_data/CSIRO/GI/baroosa_ext_WGS_buff3.shp")
-barrossa_sf <- as(barrossa_st, "Spatial") #convert to a sp object
-
-
-mean_GDD_all_yrs_c <- crop(mean_GDD_all_yrs, barrossa_sf)
-mean_GDD_all_yrs_c
-plot(mean_GDD_all_yrs_c)
-
-mean_GDD_all_yrs_m <- mask(mean_GDD_all_yrs_c, barrossa_sf)
-mean_GDD_all_yrs_m
-plot(mean_GDD_all_yrs_m)
-
+plot(mean_GDD_all_yrs)
 
 # #Write
-writeRaster(mean_GDD_all_yrs_c, "//FSSA2-ADL/CLW-SHARE3/Viticulture/Barossa terroir/climate/Climate+Forecast+Data+aggregation/map_layers/mean_GDD_all_yrs_1989_2018",format = "GTiff", overwrite = TRUE) 
-
-
+writeRaster(mean_GDD_all_yrs, "//FSSA2-ADL/CLW-SHARE3/Viticulture/Barossa terroir/climate/Climate+Forecast+Data+aggregation/map_layers/mean_GDD_all_yrs_1989_2018",format = "GTiff", overwrite = TRUE) 
 
 
 ######################################################################################################################
@@ -191,16 +176,6 @@ writeRaster(mean_GDD_all_yrs_c, "//FSSA2-ADL/CLW-SHARE3/Viticulture/Barossa terr
 #########################################################################################################################
 
 
-##### clip the grids for each year to the study area
-GDD_all_yrs
-
-GDD_all_yrs_c <- crop(GDD_all_yrs, barrossa_sf)
-GDD_all_yrs_c
-plot(GDD_all_yrs_c)
-
-GDD_all_yrs_m <- mask(GDD_all_yrs_c, barrossa_sf)
-GDD_all_yrs_m
-plot(GDD_all_yrs_m)
 
 ##### bring in and use a shapefile which conatins the points I want to extract
 
@@ -224,17 +199,12 @@ head(GDD_all_yrs_extract)
 GDD_all_yrs_wide <- data.frame(barrossa_extract_sf$POINT_X, barrossa_extract_sf$POINT_Y, GDD_all_yrs_extract)
 head(GDD_all_yrs_wide)
 #I am not confiedent in the order think it might be leap year first and then non leap years
-#names(GDD_all_yrs_wide) <- c("POINT_X", "POINT_Y", "1989", "1990", "1991", "1992", "1993", "1994",
-#                              "1995", "1996", "1997", "1998", "1999", "2000",
-#                              "2001", "2002", "2003", "2004", "2005", "2006",
-#                              "2007", "2008", "2009", "2010", "2011", "2012",
-#                              "2013", "2014", "2015", "2016", "2017", "2018")
+names(GDD_all_yrs_wide) <- c("POINT_X", "POINT_Y", "1989", "1990", "1991", "1992", "1993", "1994",
+                              "1995", "1996", "1997", "1998", "1999", "2000",
+                              "2001", "2002", "2003", "2004", "2005", "2006",
+                              "2007", "2008", "2009", "2010", "2011", "2012",
+                              "2013", "2014", "2015", "2016", "2017", "2018")
 
-names(GDD_all_yrs_wide) <- c("POINT_X", "POINT_Y", "1992", "1996", "2000", "2004", "2008", "2012",
-                             "2016", "1989", "1990", "1991", "1993", "1994",
-                             "1995", "1997", "1998", "1999", "2001", "2002",
-                             "2003", "2005", "2006", "2007", "2009", "2010",
-                             "2011", "2013", "2014", "2015", "2017", "2018")
 
 
 head(GDD_all_yrs_wide)
@@ -262,8 +232,8 @@ GDD_all_yrs_narrow <- mutate(GDD_all_yrs_narrow, year_as_double = as.double(year
 
 ggplot(GDD_all_yrs_narrow, aes(factor(year_as_double), GDD_all))+
   geom_boxplot()+
-  #geom_smooth(method = "lm", se=FALSE, color="black", aes(group=1))+ #straight line regression
-  geom_smooth(color="black", aes(group=1))+ #smooth line
+  geom_smooth(method = "lm", se=FALSE, color="black", aes(group=1))+ #straight line regression
+  #geom_smooth(color="black", aes(group=1))+ #smooth line
   theme_classic()+
   theme(axis.text.x = element_text(angle = 90, hjust=1),
         plot.caption = element_text(hjust = 0))+
